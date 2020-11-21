ad36139e8c5b13d3783027e681710451525bfbec
Sebuhi <sebuhiheziyev39@gmail.com>
2020-11-20 16:07:22 -0800

    Responsiv index html

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+$(window).on('load', function () {
+    // load-gif
+    $(pereloder).addClass("pereloder-delet");
+    setTimeout(function() { 
+         $(pereloder).addClass("d-none");
+    }, 300);
+
+
+    //responsive
+    var $size = $(window).width();
+    if($size  < 768){
+        //first section responsive
+        $(".big-img").addClass("d-none");
+        $(".little-img").first().removeClass("d-none");
+
+        //header-responsive
+        $("#head-deskopt .socail-media").addClass("d-none");
+        $("#head-deskopt .logo-text").addClass("d-none");
+        
+        //navbar responsive
+        $(".menu-bars").removeClass("d-none");
+        $(".menu").addClass("responsive-nav d-none");
+    }
+    else{
+        //first section responsive
+        $(".big-img").removeClass("d-none");
+        $(".little-img").first().addClass("d-none");
+    }
+
+
+    $( ".menu-bars" ).click(function() {
+        $(".menu").toggleClass("d-none");
+    });
+});
+
+
